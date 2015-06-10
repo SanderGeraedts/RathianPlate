67140fea5505ac6e6f6a9a931c5f466747cf696b
Sander Geraedts <sandergeraedts@live.nl>
2015-06-10 10:10:19 +0200

    Message en Monster
    De Message en Monster classes geimplementeerd

--- RathianPlate/RathianPlate/Message.cs
+++ RathianPlate/RathianPlate/Message.cs
@@ -8,14 +8,35 @@ namespace RathianPlate
     public class Message
     {
         #region fields
+
+        private int id;
+        private DateTime sentOn;
+        private string text;
+
         #endregion
+
         #region properties
+        public int Id { get { return id; } }
+        public DateTime SentOn { get { return sentOn; } }
+        public string Text { get { return text; } }
         #endregion
+
         #region constructor
+
+        public Message(int id, DateTime sentOn, string text)
+        {
+            this.id = id;
+            this.sentOn = sentOn;
+            this.text = text;
+        }
         #endregion
+
         #region private methods
+
         #endregion
+
         #region public methods
+
         #endregion
     }
 }
--- RathianPlate/RathianPlate/Monster.cs
+++ RathianPlate/RathianPlate/Monster.cs
@@ -10,15 +10,31 @@ namespace RathianPlate
         #region fields
 
         private int id;
+        private string name;
+        private string rank;
+        private string signatureMove;
+        private string description;
 
         #endregion
 
         #region properties
-
+        public int Id { get { return id; } }
+        public string Name { get { return name; } }
+        public string Rank { get { return rank; } }
+        public string SignatureMove { get { return signatureMove; } }
+        public string Description { get { return description; } }
         #endregion
 
         #region constructor
 
+        public Monster(int id, string name, string rank, string signatureMove, string description)
+        {
+            this.id = id;
+            this.name = name;
+            this.rank = rank;
+            this.signatureMove = signatureMove;
+            this.description = description;
+        }
         #endregion
 
         #region private methods
